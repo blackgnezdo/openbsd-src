220776d8699c4088ef7961fe5c57a44233fb9640
Greg Steuck <greg@nest.cx>
2024-07-12 08:02:45 -0700

    Original patch

--- sys/kern/init_main.c
+++ sys/kern/init_main.c
@@ -74,6 +74,10 @@
 #include <sys/smr.h>
 #include <sys/evcount.h>
 
+#ifdef KASAN
+#include <sys/kasan.h>
+#endif
+
 #include <sys/syscallargs.h>
 
 #include <uvm/uvm_extern.h>
@@ -430,6 +434,7 @@ main(void *framep)
 		if (fork1(p, FORK_FORK, start_init, NULL, NULL, &initproc))
 			panic("fork init");
 		initprocess = initproc->p_p;
+printf("DEBUG PARENT: kasan_in_init=%d\n", kasan_in_init);
 	}
 
 	/*
@@ -567,6 +572,9 @@ static char *initpaths[] = {
 };
 
 void
+#ifdef KASAN
+__attribute__((no_sanitize("address")))
+#endif
 check_console(struct proc *p)
 {
 	struct nameidata nd;
@@ -588,6 +596,9 @@ check_console(struct proc *p)
  * The program is invoked with one argument containing the boot flags.
  */
 void
+#ifdef KASAN
+__attribute__((no_sanitize("address")))
+#endif
 start_init(void *arg)
 {
 	struct proc *p = arg;
@@ -606,6 +617,9 @@ start_init(void *arg)
 	/*
 	 * Now in process 1.
 	 */
+#ifdef KASAN
+	kasan_in_init = 1;
+#endif
 
 	/*
 	 * Wait for main() to tell us that it's safe to exec.
@@ -719,6 +733,9 @@ start_init(void *arg)
 		 */
 		if ((error = sys_execve(p, &args, retval)) == EJUSTRETURN) {
 			KERNEL_UNLOCK();
+#ifdef KASAN
+			kasan_in_init = 0;
+#endif
 			return;
 		}
 		if (error != ENOENT)
