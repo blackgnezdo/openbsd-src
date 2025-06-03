d924a594c2d2b90d7954fef7b1b6f57a87b9e358
mbuhl <git@moritzbuhl.de>
2025-06-03 11:11:58 +0200

    Readd direct map allocations.

--- sys/uvm/uvm_km.c
+++ sys/uvm/uvm_km.c
@@ -579,10 +579,8 @@ km_alloc(size_t sz, const struct kmem_va_mode *kv,
 			TAILQ_REMOVE(&pgl, pg, pageq);
 			va = pmap_map_direct(pg);
 #ifdef KASAN
-/* XXX: how to handle direct map?
 			kasan_enter_shad_multi(va, PAGE_SIZE);
 			kasan_alloc(va, (kp->kp_zero) ? PAGE_SIZE : 0, PAGE_SIZE);
-*/
 #endif
 			if (sva == 0)
 				sva = va;
