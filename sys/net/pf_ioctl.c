ea4af2a362c46b7d033834f480077648e00dc7eb
Greg Steuck <greg@nest.cx>
2026-07-27 03:41:29 -0700

    Null-terminate strings in DIOCADDQUEUE
    Reported-by: syzbot+a6aee90c45b70ad6dde5@syzkaller.appspotmail.com
    
    Kasan-Gate: host shadow test ok; KASAN_SYZKALLER boot clean to login:; negtest 16/16

--- sys/net/pf_ioctl.c
+++ sys/net/pf_ioctl.c
@@ -2218,6 +2218,9 @@ pfioctl(dev_t dev, u_long cmd, caddr_t addr, int flags, struct proc *p)
 			goto fail;
 		}
 		memcpy(qs, &q->queue, sizeof(*qs));
+		qs->qname[sizeof(qs->qname) - 1] = '\0';
+		qs->parent[sizeof(qs->parent) - 1] = '\0';
+		qs->ifname[sizeof(qs->ifname) - 1] = '\0';
 		qs->qid = pf_qname2qid(qs->qname, 1);
 		if (qs->qid == 0) {
 			error = EBUSY;
